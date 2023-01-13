2ad24cefb04d2bd003e5c6ce719e8ee452d26def
harish khope <harishkhope89@gmail.com>
2023-01-13 18:46:19 +0530

    modified levels of questions

---
+++ Platforms/LeetCode/Arrays/017.Remove_Duplicates_from _Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+         int n = nums.size(), j = 0;
+        for(int i=1; i<n; i++)
+        {
+            if(nums[j] != nums[i])
+            {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j+1;
+        
+    }
+};
